5e7e3c33a252e01d27a6cf53278fc9025d030d35
Lucas Laguilio <laguiliolucas@gmail.com>
2026-09-22 12:51:45 -0300

    Feature: add better error handling

--- src/Controller.py
+++ src/Controller.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 from datetime import datetime
 
 from pynput.keyboard import Key as KeyClass
@@ -11,9 +12,14 @@ SERVER_IP = "192.168.0.102"
 SERVER_PORT = 5005
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.settimeout(0.5)
-client_socket.connect((SERVER_IP, SERVER_PORT))
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.settimeout(0.5)
+    client_socket.connect((SERVER_IP, SERVER_PORT))
+except OSError as e:
+    print(f"Erro ao abrir socket: {e}")
+
+
 keys_pressed = set()
 
 
@@ -24,11 +30,13 @@ def send_keys():
         time_str = datetime.now()
         client_socket.send(key_str.encode("utf-8"))
         data, _addr = client_socket.recvfrom(1024)
-        print(f"time_sent: {time_str} | Server response: {data.decode('utf-8')}  ")
-    except TimeoutError:
-        print("Request timed out. No response from server, trying again...")
-        send_keys()
-        
+        print(f"Resposta do servidor: {data} - {time_str}")
+    except (TimeoutError, OSError) as e:
+        if e == "timed out":
+            print("Aviso: Tempo limite de resposta do servidor excedido (Timeout)")
+        else:
+            print(f"Erro: {e}")
+            sys.exit(1)
 
 
 def on_press(key):
@@ -46,18 +54,21 @@ def on_press(key):
 
     except Exception as e:  # noqa
         print(f"Ocorreu um erro: {e}")
+        sys.exit(1)
+
 
 
 def on_release(key):
-    if hasattr(key, "char") and key.char is not None:
-        if key.char in ("w", "a", "s", "d"):
+    if hasattr(key, "char") and key.char is not None and key.char in ("w", "a", "s", "d"):
             keys_pressed.discard(key.char)
             send_keys()
-
     if key == KeyClass.esc:
-        print("Closing the listener...")
-        return False
+        print("Fechando o controlador.")
+        sys.exit(1)
 
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+    try:
+        listener.join()
+    except Exception as e:  # noqa
+        print(f"Erro: {e}")
